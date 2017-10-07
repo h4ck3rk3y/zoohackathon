7bff3479a6d3b20215e1d76d1ad2219f36f23cbf
Gyanendra Mishra <anomaly.the@gmail.com>
2017-10-07 18:03:00 +0100

    New rangers

---
+++ Rangers.py
@@ -0,0 +1,25 @@
+from scipy import spatial
+from collections import defaultdict
+import csv
+
+class Rangers:
+
+	def __init__(self, rangersFile="Rangers.csv"):
+		self.rangersFile = rangersFile
+		self.rangers = defaultdict(list)
+		self.rangerPos = []
+		self.kdtree = []
+		self.load()
+
+	def load(self):
+		with open(self.rangersFile, 'rb') as rfile:
+			rangerReader = csv.reader(rfile, delimiter=',', quotechar='|')
+			for row in rangerReader:
+				self.rangers[(float(row[0]), float(row[1]))].append({'name': row[2], 'number': row[3]})
+		self.rangerPos = self.rangers.keys()
+		self.kdtree = spatial.KDTree(self.rangerPos)
+
+	def whoToCall(self, alerLoc):
+		distance, index = self.kdtree.query(alerLoc)
+		pair = self.rangerPos[index]
+		return self.rangers[pair][0]
