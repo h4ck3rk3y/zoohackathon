4dc3833c51583ceb9c02e11752fc16d4c997f035
Gyanendra Mishra <anomaly.the@gmail.com>
2017-10-08 11:54:20 +0100

    removed dependence on scipy

--- Rangers.py
+++ Rangers.py
@@ -1,4 +1,4 @@
-from scipy import spatial
+from functools import partial
 from collections import defaultdict
 import csv
 import sqlite3
@@ -13,7 +13,6 @@ class Rangers:
 		self.rangersFile = rangersFile
 		self.rangers = defaultdict(list)
 		self.rangerPos = []
-		self.kdtree = []
 		self.load()
 
 	def load(self):
@@ -22,11 +21,13 @@ class Rangers:
 			for row in rangerReader:
 				self.rangers[(float(row[0]), float(row[1]))].append({'name': row[2], 'number': row[3]})
 		self.rangerPos = self.rangers.keys()
-		self.kdtree = spatial.KDTree(self.rangerPos)
+
+	def findClosest(self, listOfRangers, ranger):
+		dist=lambda s,d: (s[0]-d[0])**2+(s[1]-d[1])**2
+		return min(listOfRangers, key=partial(dist, ranger))
 
 	def whoToCall(self, alerLoc):
-		distance, index = self.kdtree.query(alerLoc)
-		pair = self.rangerPos[index]
+		pair = self.findClosest(self.rangerPos, alerLoc)
 		return self.rangers[pair][0]
 
 	def whoToCallDB(self, alerLoc):
@@ -38,9 +39,7 @@ class Rangers:
 			rangers[(float(row[0]), float(row[1]))].append({'name': row[2], 'number': row[3]})
 		if rangers:
 			pos = rangers.keys()
-			kdtree = spatial.KDTree(pos)
-			distance, index = kdtree.query(alerLoc)
-			pair = pos[index]
+			pair = self.findClosest(pos, alerLoc)
 			return rangers[pair][0]
 		else:
 			return self.whoToCall(alerLoc)
