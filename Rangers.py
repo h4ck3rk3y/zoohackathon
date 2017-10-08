fac6f1d5bdfa667dbc461c48f925219783ca0752
Gyanendra Mishra <anomaly.the@gmail.com>
2017-10-08 09:14:42 +0100

    updated rangers to fetch from db

--- Rangers.py
+++ Rangers.py
@@ -1,6 +1,11 @@
 from scipy import spatial
 from collections import defaultdict
 import csv
+import sqlite3
+
+def get_db():
+    db = sqlite3.connect('database.db', timeout=10)
+    return db
 
 class Rangers:
 
@@ -23,3 +28,16 @@ class Rangers:
 		distance, index = self.kdtree.query(alerLoc)
 		pair = self.rangerPos[index]
 		return self.rangers[pair][0]
+
+	def whoToCallDB(self, alerLoc):
+		query = "select * from rangers"
+		cur = get_db().execute(query, [])
+		rangers = cur.fetchall()
+		rangers = defaultdict(list)
+		for ranger in rangers:
+			rangers[(float(row[0]), float(row[1]))].append({'name': row[2], 'number': row[3]})
+		pos = rangers.keys()
+		kdtree = spatial.kdtree(pos)
+		distance, index = kdtree.query(alerLoc)
+		pair = pos[index]
+		return rangers[pair][0]
